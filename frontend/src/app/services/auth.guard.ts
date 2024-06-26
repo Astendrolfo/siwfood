5bdcf4d94265c90f36e12d9ad17c0b3a956e0fdc
Astendrolfo <martisemilio12@gmail.com>
2024-06-26 19:08:12 +0200

    Fix del check dell'url da parte dell'AuthGuard

--- frontend/src/app/services/auth.guard.ts
+++ frontend/src/app/services/auth.guard.ts
@@ -1,20 +1,30 @@
-import {inject} from "@angular/core";
-import {CanActivateFn, Router} from "@angular/router";
+import { inject } from "@angular/core";
+import { CanActivateFn, Router, ActivatedRouteSnapshot } from "@angular/router";
 
 export const authGuard: (protectedRoutes: string[]) => CanActivateFn = (protectedRoutes: string[]) => {
-  return () => {
+  return (route: ActivatedRouteSnapshot) => {
     const router = inject(Router);
-    const currentRoute = router.url;
-    console.log(currentRoute);
-    const token = typeof window !== 'undefined' ? localStorage.getItem('token') : null
-    if (token) return true; // Ho un token, accedo alla pagina e verifico il token.
-    else if (protectedRoutes.includes(currentRoute)) {
-        router.navigateByUrl("/login").then();
-      }
-    console.log(router.url)
-    return false;
 
+    //Bisogna usare ActivatedRouteSnapshot per ottenere l'url successiva.
+
+    /* Il guard viene attivato ancora prima di entrare nella componente, quindi se chiedessi l'url al router, mi
+    restituirebbe l'url della componente precedente */
+
+    const currentRoute = "/" + route.url.map(segment => segment.path).join();
+
+    // Check del token senza andare in errore
+
+    const token = typeof window !== 'undefined' ? localStorage.getItem('token') : null;
+
+    if (token) {
+      return true;
+    } else if (protectedRoutes.includes(currentRoute)) {
+      router.navigateByUrl("/login");
+      return false;
+    }
+    return true;
   };
-}
-const protectedRoutes: string[] = ['/home', '/profile'];
+};
+
+const protectedRoutes: string[] = ['/profile'];
 export const canActivate = authGuard(protectedRoutes);
